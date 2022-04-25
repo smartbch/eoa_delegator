bcee9fe4ab248427542d3553b2ab326407fea325
Kui Wang <wangkui0508@hotmail.com>
2022-04-25 17:25:13 +0800

    save some gas when revert

--- contracts/eoa_delegator.sol
+++ contracts/eoa_delegator.sol
@@ -51,10 +51,10 @@ contract EOADelegator {
             calldatacopy(callRetPtr, 0, inputSize)
             let result := call(gas(), contractAddr, callvalue(), callRetPtr, inputSize, 0, 0)
             retSize := returndatasize()
-            returndatacopy(callRetPtr, 0, retSize)
             if eq(result, 0) {
                 revert(callRetPtr, retSize)
             }
+            returndatacopy(callRetPtr, 0, retSize)
             mstore(0x40, add(callRetPtr, retSize))
         }
         for (uint j = 0; j < tokenNums; j++) {
